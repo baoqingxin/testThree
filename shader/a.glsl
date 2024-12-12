5852b133e0a3c5c6322f580d9058f80aee17e70e
bqx <1142634487@qq.com>
2024-12-12 18:41:19 +0800

    init

---
+++ shader/a.glsl
@@ -0,0 +1,17 @@
+// int a = 123;
+// float b = 1.0;
+// bool c = true;
+
+// 二维向量vec2
+// vec2 foo = vec2(1.0， 2.0) // 会报错
+
+// vec2 foo = vec2(0.0)
+// foo.x = 1.0
+// foo.y = 2.0
+
+
+// float loremIpsum () {
+//     float a = 1.0
+//     float b = 2.0
+//     return a + b
+// }
